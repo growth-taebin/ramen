416f251d68506f3404ed72d2cb670f61dd5687a7
yoontaebin <gusckd000@gmail.com>
2023-02-20 23:37:00 +0900

    project setting

---
+++ src/main/kotlin/com/example/ramenbm/RamenbmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ramenbm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RamenbmApplication
+
+fun main(args: Array<String>) {
+	runApplication<RamenbmApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ramenbm/RamenbmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ramenbm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RamenbmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
