bc587c270c387cd2d174d995c0cbfdd9059705f2
yoontaebin <gusckd000@gmail.com>
2023-02-22 17:58:39 +0900

    ✨  이메일 중복 예외 추가

---
+++ src/main/kotlin/com/example/ramenbm/global/exception/RamenException.kt
@@ -0,0 +1,5 @@
+package com.example.ramenbm.global.exception
+
+import com.example.ramenbm.global.exception.enumType.ErrorCode
+
+open class RamenException(val errorCode: ErrorCode): RuntimeException(errorCode.message)
