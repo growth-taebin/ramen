7a84fa1c6413072069f2fc7261f853121d0082d7
yoontaebin <gusckd000@gmail.com>
2023-02-24 20:28:55 +0900

    :art: toEntity 메서드 추가

--- src/main/kotlin/com/example/ramenbm/domain/user/presentation/data/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/presentation/data/dto/request/SignUpRequest.kt
@@ -1,8 +1,21 @@
 package com.example.ramenbm.domain.user.presentation.data.dto.request
 
+import com.example.ramenbm.domain.user.entity.User
+
 data class SignUpRequest(
         val email: String,
         val password: String,
         val name: String,
-        val discord: String
+        val discord: String,
 )
+
+fun SignUpRequest.toEntity(encodePassword: String) =
+        User(
+                id = -1,
+                email = email,
+                password = encodePassword,
+                name = name,
+                discord = discord
+        )
+
+
--- src/main/kotlin/com/example/ramenbm/domain/user/service/UserAuthService.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/UserAuthService.kt
@@ -1,8 +1,8 @@
 package com.example.ramenbm.domain.user.service
 
-import com.example.ramenbm.domain.user.entity.User
 import com.example.ramenbm.domain.user.exception.DuplicateEmailException
 import com.example.ramenbm.domain.user.presentation.data.dto.request.SignUpRequest
+import com.example.ramenbm.domain.user.presentation.data.dto.request.toEntity
 import com.example.ramenbm.domain.user.repository.UserRepository
 import com.example.ramenbm.global.annotation.ServiceWithTransaction
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -17,7 +17,7 @@ class UserAuthService(
         if(userRepository.existsByEmail(request.email)) {
             throw DuplicateEmailException()
         }
-        userRepository.save(User(id = -1, request.email, passwordEncoder.encode(request.password), request.name, request.discord))
+        userRepository.save(request.toEntity(passwordEncoder.encode(request.password)))
 
     }
 }
