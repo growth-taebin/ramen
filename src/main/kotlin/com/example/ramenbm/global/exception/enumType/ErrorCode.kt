604b97cf6e787b19858fb2d7849016f61f0f6097
yoontaebin <gusckd000@gmail.com>
2023-03-02 18:03:28 +0900

    :sparkles: user exception 추가

--- src/main/kotlin/com/example/ramenbm/global/exception/enumType/ErrorCode.kt
+++ src/main/kotlin/com/example/ramenbm/global/exception/enumType/ErrorCode.kt
@@ -5,5 +5,9 @@ enum class ErrorCode(
         val status: Int
 ) {
 
-    DUPLICATE_EMAIL("중복된 이메일 입니다", 409)
+    DUPLICATE_EMAIL("중복된 이메일 입니다", 409),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다", 404),
+
+    ACCESS_TOKEN_EXPIRED("만료된 accessToken 입니다.", 401),
+    INVALID_TOKEN("유효하지 않은 토큰입니다.", 401)
 }
