521360bc42f6e8f11899c1ad420721613dae6793
yoontaebin <gusckd000@gmail.com>
2023-03-02 17:02:02 +0900

    :sparkles: 인증객체 생성 클래스 추가

---
+++ src/main/kotlin/com/example/ramenbm/global/authentication/AuthDetails.kt
@@ -0,0 +1,24 @@
+package com.example.ramenbm.global.authentication
+
+import com.example.ramenbm.domain.user.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.Collections
+
+class AuthDetails(
+        private val user: User
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = Collections.emptyList()
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = user.email
+
+    override fun isAccountNonExpired(): Boolean = false
+
+    override fun isAccountNonLocked(): Boolean = false
+
+    override fun isCredentialsNonExpired(): Boolean = false
+
+    override fun isEnabled(): Boolean = false
+}
