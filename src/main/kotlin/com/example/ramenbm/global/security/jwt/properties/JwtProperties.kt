9c4a09a113ec5aeaaba5a417b154c37d1774e4b2
yoontaebin <gusckd000@gmail.com>
2023-03-02 18:30:26 +0900

    :sparkles: jwtproperties 추가

---
+++ src/main/kotlin/com/example/ramenbm/global/security/jwt/properties/JwtProperties.kt
@@ -0,0 +1,17 @@
+package com.example.ramenbm.global.security.jwt.properties
+
+import io.jsonwebtoken.security.Keys
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.nio.charset.StandardCharsets
+import java.security.Key
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties(
+        accessKey: String,
+        refreshKey: String
+) {
+    val accessSecret: Key = Keys.hmacShaKeyFor(accessKey.toByteArray(StandardCharsets.UTF_8))
+    val refreshSecret: Key = Keys.hmacShaKeyFor(refreshKey.toByteArray(StandardCharsets.UTF_8))
+}
