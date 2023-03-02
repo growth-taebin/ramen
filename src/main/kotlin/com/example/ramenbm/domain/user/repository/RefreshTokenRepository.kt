f0bc8bdd5b899670c2568ab1844525bcc619f5ff
yoontaebin <gusckd000@gmail.com>
2023-03-02 15:37:52 +0900

    :sparkles: RefreshToken entity 추가

---
+++ src/main/kotlin/com/example/ramenbm/domain/user/repository/RefreshTokenRepository.kt
@@ -0,0 +1,7 @@
+package com.example.ramenbm.domain.user.repository
+
+import com.example.ramenbm.domain.user.entity.RefreshToken
+import org.springframework.data.repository.CrudRepository
+
+interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
+}
