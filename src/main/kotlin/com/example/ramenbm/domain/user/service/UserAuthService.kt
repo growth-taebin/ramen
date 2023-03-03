78f47ed0f3b413648a81e8b8362712a0929cb0e2
yoontaebin <gusckd000@gmail.com>
2023-03-03 10:26:54 +0900

    :sparkles: 로그인 서비스 구현

--- src/main/kotlin/com/example/ramenbm/domain/user/service/UserAuthService.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/UserAuthService.kt
@@ -1,8 +1,11 @@
 package com.example.ramenbm.domain.user.service
 
+import com.example.ramenbm.domain.user.presentation.data.dto.request.SignInRequest
 import com.example.ramenbm.domain.user.presentation.data.dto.request.SignUpRequest
+import com.example.ramenbm.domain.user.presentation.data.dto.response.SignInResponse
 
 interface UserAuthService {
 
     fun signup(request: SignUpRequest): Long
+    fun signin(request: SignInRequest): SignInResponse
 }
--- src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAuthServiceImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAuthServiceImpl.kt
@@ -1,17 +1,24 @@
 package com.example.ramenbm.domain.user.service.impl
 
+import com.example.ramenbm.domain.user.entity.User
 import com.example.ramenbm.domain.user.exception.DuplicateEmailException
+import com.example.ramenbm.domain.user.exception.PasswordNotCorrectException
+import com.example.ramenbm.domain.user.exception.UserNotFoundException
+import com.example.ramenbm.domain.user.presentation.data.dto.request.SignInRequest
 import com.example.ramenbm.domain.user.presentation.data.dto.request.SignUpRequest
 import com.example.ramenbm.domain.user.presentation.data.dto.request.toEntity
+import com.example.ramenbm.domain.user.presentation.data.dto.response.SignInResponse
 import com.example.ramenbm.domain.user.repository.UserRepository
 import com.example.ramenbm.domain.user.service.UserAuthService
 import com.example.ramenbm.global.annotation.ServiceWithTransaction
+import com.example.ramenbm.global.security.jwt.TokenProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @ServiceWithTransaction
 class UserAuthServiceImpl(
         private val userRepository: UserRepository,
-        private val passwordEncoder: PasswordEncoder
+        private val passwordEncoder: PasswordEncoder,
+        private val tokenProvider: TokenProvider
 ): UserAuthService {
 
     override fun signup(request: SignUpRequest): Long {
@@ -20,4 +27,14 @@ class UserAuthServiceImpl(
         }
         return userRepository.save(request.toEntity(passwordEncoder.encode(request.password))).idx
     }
+
+    override fun signin(request: SignInRequest): SignInResponse {
+        val user: User = userRepository.findByEmail(request.email) ?: throw UserNotFoundException()
+        if (!passwordEncoder.matches(request.password, user.password)) {
+            throw PasswordNotCorrectException()
+        }
+        return tokenProvider.generate(request.email)
+    }
+
+
 }
