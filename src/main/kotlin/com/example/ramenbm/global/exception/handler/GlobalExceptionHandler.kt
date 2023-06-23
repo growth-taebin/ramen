7062857affd9e88ab6162e281d500c2ce639b513
yoontaebin <gusckd000@gmail.com>
2023-06-23 13:58:00 +0900

    :ambulance: RestControllerAdvice 어노테이션 추가

--- src/main/kotlin/com/example/ramenbm/global/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/ramenbm/global/exception/handler/GlobalExceptionHandler.kt
@@ -5,7 +5,9 @@ import com.example.ramenbm.global.exception.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
+@RestControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(RamenException::class)
