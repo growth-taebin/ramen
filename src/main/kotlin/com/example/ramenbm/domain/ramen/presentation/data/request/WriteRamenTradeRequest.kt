b45e4012b93c4e93bfe44e3e6aa3031ef7a59ccc
yoontaebin <gusckd000@gmail.com>
2023-06-27 08:57:26 +0900

    :art: validation 추가

---
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/presentation/data/request/WriteRamenTradeRequest.kt
@@ -0,0 +1,15 @@
+package com.example.ramenbm.domain.ramen.presentation.data.request
+
+import com.example.ramenbm.domain.ramen.entity.RamenTrade
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class WriteRamenTradeRequest(
+    @field:NotBlank
+    val title: String,
+    @field:NotNull
+    val count: Int,
+    @field:NotBlank
+    val price: String
+)
+
