f669ee3c931cc452c8199d685b28f1352ba8b46b
yoontaebin <gusckd000@gmail.com>
2023-06-27 20:13:20 +0900

    :recycle: CrudRepository로 변경

--- src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
@@ -2,7 +2,8 @@ package com.example.ramenbm.domain.ramen.repository
 
 import com.example.ramenbm.domain.ramen.entity.RamenTrade
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RamenTradeRepository: JpaRepository<RamenTrade, Long>
+interface RamenTradeRepository: CrudRepository<RamenTrade, Long>
