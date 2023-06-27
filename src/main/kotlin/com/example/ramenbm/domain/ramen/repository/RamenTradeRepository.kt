c4a0642f0ab5b8bf776315497cb5f11037bcd49e
yoontaebin <gusckd000@gmail.com>
2023-06-27 20:14:41 +0900

    :recycle: @Repository 삭제

--- src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
-@Repository
 interface RamenTradeRepository: CrudRepository<RamenTrade, Long>
