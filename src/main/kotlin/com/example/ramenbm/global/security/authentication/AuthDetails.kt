a5b994f2187b222e5c3c3cc5893ec1ed1da2529f
yoontaebin <gusckd000@gmail.com>
2023-06-29 22:20:46 +0900

    :recycle: email 매개변수로 받아오게 변경

--- src/main/kotlin/com/example/ramenbm/global/security/authentication/AuthDetails.kt
+++ src/main/kotlin/com/example/ramenbm/global/security/authentication/AuthDetails.kt
@@ -1,20 +1,19 @@
 package com.example.ramenbm.global.security.authentication
 
-import com.example.ramenbm.domain.user.entity.User
 import com.example.ramenbm.domain.user.type.Authority
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class AuthDetails(
-    private val user: User
+    private val email: String
 ) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
         mutableListOf(SimpleGrantedAuthority(Authority.ROLE_USER.name))
 
     override fun getPassword(): String? = null
 
-    override fun getUsername(): String = user.email
+    override fun getUsername(): String = email
 
     override fun isAccountNonExpired(): Boolean = false
 
