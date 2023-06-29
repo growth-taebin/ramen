55bf943a3dc5c4992005f3907323b83454b8ec67
yoontaebin <gusckd000@gmail.com>
2023-06-29 17:15:36 +0900

    :sparkles: findRamenTradeByIdx 메서드 추가

--- src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/repository/RamenTradeRepository.kt
@@ -1,8 +1,10 @@
 package com.example.ramenbm.domain.ramen.repository
 
 import com.example.ramenbm.domain.ramen.entity.RamenTrade
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 
-interface RamenTradeRepository: CrudRepository<RamenTrade, Long>
+interface RamenTradeRepository: CrudRepository<RamenTrade, Long> {
+
+    fun findRamenTradeByIdx(idx: Long): RamenTrade?
+
+}
