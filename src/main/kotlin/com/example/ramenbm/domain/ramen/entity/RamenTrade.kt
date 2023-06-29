1ab23ab134e20978615fd647710b2d6d1885bbe1
yoontaebin <gusckd000@gmail.com>
2023-06-29 17:11:07 +0900

    :sparkles: updateRamenTrade 메서드 추가

--- src/main/kotlin/com/example/ramenbm/domain/ramen/entity/RamenTrade.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/entity/RamenTrade.kt
@@ -1,5 +1,6 @@
 package com.example.ramenbm.domain.ramen.entity
 
+import com.example.ramenbm.domain.ramen.presentation.data.dto.UpdateRamenTradeDto
 import com.example.ramenbm.domain.user.entity.User
 import com.example.ramenbm.global.entity.BaseIdxEntity
 import javax.persistence.Entity
@@ -8,10 +9,17 @@ import javax.persistence.ManyToOne
 
 @Entity
 class RamenTrade(
-    val title: String,
-    val count: Int,
-    val price: Int,
-    val content: String,
+    var title: String,
+    var count: Int,
+    var price: Int,
+    var content: String,
     @ManyToOne(fetch = FetchType.LAZY)
     val user: User
-): BaseIdxEntity()
+): BaseIdxEntity() {
+    fun updateRamenTrade(dto: UpdateRamenTradeDto) {
+        this.title = dto.title
+        this.count = dto.count
+        this.price = dto.price
+        this.content = dto.content
+    }
+}
