a1490868af1db1da37b165ff043cc79de907dadd
yoontaebin <gusckd000@gmail.com>
2023-06-29 17:14:37 +0900

    :sparkles: UpdateRamenTradeDto 추가

--- src/main/kotlin/com/example/ramenbm/domain/ramen/util/RamenTradeConverter.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/util/RamenTradeConverter.kt
@@ -1,12 +1,18 @@
 package com.example.ramenbm.domain.ramen.util
 
 import com.example.ramenbm.domain.ramen.entity.RamenTrade
-import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.dto.UpdateRamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.dto.WriteRamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.request.UpdateRamenTradeRequest
 import com.example.ramenbm.domain.ramen.presentation.data.request.WriteRamenTradeRequest
 import com.example.ramenbm.domain.user.entity.User
 
 interface RamenTradeConverter {
 
-    fun toDto(request: WriteRamenTradeRequest): RamenTradeDto
-    fun toEntity(dto: RamenTradeDto, user: User): RamenTrade
+    fun toDto(request: WriteRamenTradeRequest): WriteRamenTradeDto
+
+    fun toDto(idx: Long, request: UpdateRamenTradeRequest): UpdateRamenTradeDto
+
+    fun toEntity(dto: WriteRamenTradeDto, user: User): RamenTrade
+
 }
--- src/main/kotlin/com/example/ramenbm/domain/ramen/util/impl/RamenTradeConverterImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/util/impl/RamenTradeConverterImpl.kt
@@ -1,7 +1,9 @@
 package com.example.ramenbm.domain.ramen.util.impl
 
 import com.example.ramenbm.domain.ramen.entity.RamenTrade
-import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.dto.UpdateRamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.dto.WriteRamenTradeDto
+import com.example.ramenbm.domain.ramen.presentation.data.request.UpdateRamenTradeRequest
 import com.example.ramenbm.domain.ramen.presentation.data.request.WriteRamenTradeRequest
 import com.example.ramenbm.domain.ramen.util.RamenTradeConverter
 import com.example.ramenbm.domain.user.entity.User
@@ -10,9 +12,12 @@ import org.springframework.stereotype.Component
 @Component
 class RamenTradeConverterImpl : RamenTradeConverter {
 
-    override fun toDto(request: WriteRamenTradeRequest): RamenTradeDto =
-        RamenTradeDto(request.title, request.count, request.price, request.content)
+    override fun toDto(request: WriteRamenTradeRequest): WriteRamenTradeDto =
+        WriteRamenTradeDto(request.title, request.count, request.price, request.content)
 
-    override fun toEntity(dto: RamenTradeDto, user: User): RamenTrade =
+    override fun toDto(idx: Long, request: UpdateRamenTradeRequest): UpdateRamenTradeDto =
+        UpdateRamenTradeDto(idx, request.title, request.count, request.price, request.content)
+
+    override fun toEntity(dto: WriteRamenTradeDto, user: User): RamenTrade =
         RamenTrade(dto.title, dto.count, dto.price, dto.content, user)
 }
