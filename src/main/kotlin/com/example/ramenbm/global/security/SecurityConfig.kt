fef7ec39a72a26887f5d2f7f837413b671508b06
yoontaebin <gusckd000@gmail.com>
2023-07-02 23:30:02 +0900

    :recycle: 토큰 없을 때 요청과 없는 엔드포인트로 요청 예외 처리 추가

--- src/main/kotlin/com/example/ramenbm/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/ramenbm/global/security/SecurityConfig.kt
@@ -2,6 +2,8 @@ package com.example.ramenbm.global.security
 
 import com.example.ramenbm.domain.user.type.Authority
 import com.example.ramenbm.global.security.filter.config.FilterConfig
+import com.example.ramenbm.global.security.handler.CustomAccessDeniedHandler
+import com.example.ramenbm.global.security.handler.CustomAuthenticationEntryPoint
 import com.example.ramenbm.global.security.jwt.TokenParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -33,12 +35,19 @@ class SecurityConfig(
             .antMatchers(HttpMethod.POST, "/auth/signup").permitAll()
             .antMatchers(HttpMethod.POST, "/auth/signin").permitAll()
             .antMatchers(HttpMethod.PATCH, "/auth/reissue").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/ramen")
-            .hasAnyAuthority(Authority.ROLE_USER.name, Authority.ROLE_ADMIN.name)
-            .antMatchers(HttpMethod.PATCH, "/api/ramen/{idx}")
-            .hasAnyAuthority(Authority.ROLE_USER.name, Authority.ROLE_ADMIN.name)
-            .anyRequest().denyAll()
+            .antMatchers(HttpMethod.POST, "/api/ramen").hasAnyAuthority(Authority.ROLE_USER.name, Authority.ROLE_ADMIN.name)
+            .antMatchers(HttpMethod.PATCH, "/api/ramen/{idx}").hasAnyAuthority(Authority.ROLE_USER.name, Authority.ROLE_ADMIN.name)
+
+            .anyRequest().permitAll()
             .and()
+
+            .exceptionHandling()
+            .accessDeniedHandler(CustomAccessDeniedHandler())
+            .and()
+            .httpBasic()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
+            .and()
+
             .apply(FilterConfig(tokenParser))
             .and()
             .build()
