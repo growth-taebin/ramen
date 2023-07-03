5dc66268b8c99979f4147c64accfaef91abe5911
yoontaebin <gusckd000@gmail.com>
2023-07-03 20:48:37 +0900

    :recycle: 엔드포인트 수정

--- src/main/kotlin/com/example/ramenbm/domain/user/presentation/UserAuthController.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/presentation/UserAuthController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/api/auth")
 class UserAuthController(
     private val userAuthService: UserAuthService
 ) {
