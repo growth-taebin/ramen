7fcfb2b06a29b676767d77c0205d4234419ecbf3
yoontaebin <gusckd000@gmail.com>
2023-07-04 18:21:58 +0900

    :sparkles: toQueryDto와 toListResponse 추가

--- src/main/kotlin/com/example/ramenbm/domain/ramen/util/RamenTradeConverter.kt
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/util/RamenTradeConverter.kt
@@ -1,10 +1,13 @@
 package com.example.ramenbm.domain.ramen.util
 
 import com.example.ramenbm.domain.ramen.entity.RamenTrade
+import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeListQueryDto
+import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeQueryDto
 import com.example.ramenbm.domain.ramen.presentation.data.dto.UpdateRamenTradeDto
 import com.example.ramenbm.domain.ramen.presentation.data.dto.WriteRamenTradeDto
 import com.example.ramenbm.domain.ramen.presentation.data.request.UpdateRamenTradeRequest
 import com.example.ramenbm.domain.ramen.presentation.data.request.WriteRamenTradeRequest
+import com.example.ramenbm.domain.ramen.presentation.data.response.RamenTradeListResponse
 import com.example.ramenbm.domain.user.entity.User
 
 interface RamenTradeConverter {
@@ -14,5 +17,9 @@ interface RamenTradeConverter {
     fun toDto(idx: Long, request: UpdateRamenTradeRequest): UpdateRamenTradeDto
 
     fun toEntity(dto: WriteRamenTradeDto, user: User): RamenTrade
+
+    fun toQueryDto(ramenTrade: RamenTrade): RamenTradeQueryDto
+
+    fun toListResponse(dto: RamenTradeListQueryDto): RamenTradeListResponse
 
 }
