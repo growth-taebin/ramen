16cb1a676701c704c4888c0e6efc43e58bdf7c95
yoontaebin <gusckd000@gmail.com>
2023-07-07 13:51:20 +0900

    :sparkles: RamenTradeDetailQueryDto 추가

---
+++ src/main/kotlin/com/example/ramenbm/domain/ramen/presentation/data/dto/RamenTradeDetailQueryDto.kt
@@ -0,0 +1,11 @@
+package com.example.ramenbm.domain.ramen.presentation.data.dto
+
+data class RamenTradeDetailQueryDto(
+        val idx: Long,
+        val title: String,
+        val count: Int,
+        val price: Int,
+        val content: String,
+        val userName: String,
+        val discord: String
+)
