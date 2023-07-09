c39e6f0bb07ef5d2f6ffea0671c21c623d1e0a88
yoontaebin <gusckd000@gmail.com>
2023-07-09 22:40:13 +0900

    :sparkles: toResponse 메서드 추가

--- src/main/kotlin/com/example/ramenbm/domain/user/util/impl/AccountConverterImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/util/impl/AccountConverterImpl.kt
@@ -2,11 +2,15 @@ package com.example.ramenbm.domain.user.util.impl
 
 import com.example.ramenbm.domain.user.entity.User
 import com.example.ramenbm.domain.user.presentation.data.dto.UserQueryDto
+import com.example.ramenbm.domain.user.presentation.data.response.UserInfoResponse
 import com.example.ramenbm.domain.user.util.AccountConverter
 
 class AccountConverterImpl : AccountConverter {
 
 	override fun toQueryDto(user: User): UserQueryDto =
 		UserQueryDto(user.idx, user.name, user.discord)
+
+	override fun toResponse(dto: UserQueryDto): UserInfoResponse =
+		UserInfoResponse(dto.name, dto.discord)
 
 }
