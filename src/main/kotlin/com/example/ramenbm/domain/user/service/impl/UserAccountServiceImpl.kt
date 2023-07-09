1965724c68de0fbb416cadf9cd7effb9e86a47be
yoontaebin <gusckd000@gmail.com>
2023-07-09 23:10:53 +0900

    :recycle: @Transactional 어노테이션 추가

--- src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
@@ -5,6 +5,7 @@ import com.example.ramenbm.domain.user.service.UserAccountService
 import com.example.ramenbm.domain.user.util.AccountConverter
 import com.example.ramenbm.domain.user.util.UserUtil
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserAccountServiceImpl(
@@ -12,6 +13,7 @@ class UserAccountServiceImpl(
 	private val accountConverter: AccountConverter
 ) : UserAccountService {
 
+	@Transactional(readOnly = true, rollbackFor = [Exception::class])
 	override fun findUserByIdx(idx: Long): UserQueryDto =
 		userUtil.findUserByIdx(idx)
 			.let { accountConverter.toQueryDto(it) }
