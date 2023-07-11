64b71d89a6834cfa146b01842d982e029f5b11b1
yoontaebin <gusckd000@gmail.com>
2023-07-11 14:49:01 +0900

    :recycle: .map으로 수정

--- src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
@@ -4,6 +4,7 @@ import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeListQuer
 import com.example.ramenbm.domain.ramen.repository.RamenTradeRepository
 import com.example.ramenbm.domain.ramen.util.RamenTradeConverter
 import com.example.ramenbm.domain.user.presentation.data.dto.UserQueryDto
+import com.example.ramenbm.domain.user.repository.UserRepository
 import com.example.ramenbm.domain.user.service.UserAccountService
 import com.example.ramenbm.domain.user.util.AccountConverter
 import com.example.ramenbm.domain.user.util.UserUtil
@@ -15,7 +16,7 @@ class UserAccountServiceImpl(
 	private val userUtil: UserUtil,
 	private val accountConverter: AccountConverter,
 	private val ramenTradeRepository: RamenTradeRepository,
-	private val ramenTradeConverter: RamenTradeConverter
+	private val ramenTradeConverter: RamenTradeConverter,
 ) : UserAccountService {
 
 	@Transactional(readOnly = true, rollbackFor = [Exception::class])
@@ -26,9 +27,9 @@ class UserAccountServiceImpl(
 	@Transactional(readOnly = true, rollbackFor = [Exception::class])
 	override fun findMyRamenTradeByUser(): RamenTradeListQueryDto {
 		val user = userUtil.currentUser()
-		ramenTradeRepository.findRamenTradeByUser(user)
-			.let { ramenTradeConverter.toListQueryDto(it) }
-			.let { return RamenTradeListQueryDto(listOf(it)) }
+		ramenTradeRepository.findAllByUser(user)
+			.map { ramenTradeConverter.toListQueryDto(it) }
+			.let { return RamenTradeListQueryDto(it) }
 	}
 
 }
