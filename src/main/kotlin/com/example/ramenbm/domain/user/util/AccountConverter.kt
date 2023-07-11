080034f2f57c46708976458d6a3dd78c985d46ed
yoontaebin <gusckd000@gmail.com>
2023-07-11 20:01:55 +0900

    :sparkles: toDto, toEntity 메서드 추가

--- src/main/kotlin/com/example/ramenbm/domain/user/util/AccountConverter.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/util/AccountConverter.kt
@@ -1,10 +1,17 @@
 package com.example.ramenbm.domain.user.util
 
 import com.example.ramenbm.domain.user.entity.User
+import com.example.ramenbm.domain.user.presentation.data.dto.UserDto
 import com.example.ramenbm.domain.user.presentation.data.dto.UserQueryDto
+import com.example.ramenbm.domain.user.presentation.data.request.SignUpRequest
 import com.example.ramenbm.domain.user.presentation.data.response.UserInfoResponse
+import com.example.ramenbm.domain.user.type.Authority
 
 interface AccountConverter {
+
+	fun toDto(request: SignUpRequest): UserDto
+
+	fun toEntity(dto: UserDto, encodePassword: String): User
 
 	fun toQueryDto(user: User): UserQueryDto
 
