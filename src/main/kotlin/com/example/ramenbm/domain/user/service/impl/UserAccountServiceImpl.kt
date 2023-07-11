de4446f1bf2ca18b3ede2f42440423dbab248996
yoontaebin <gusckd000@gmail.com>
2023-07-11 11:45:17 +0900

    :sparkles: findMyRamenTradeByUser 서비스 구현

--- src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
+++ src/main/kotlin/com/example/ramenbm/domain/user/service/impl/UserAccountServiceImpl.kt
@@ -1,5 +1,8 @@
 package com.example.ramenbm.domain.user.service.impl
 
+import com.example.ramenbm.domain.ramen.presentation.data.dto.RamenTradeListQueryDto
+import com.example.ramenbm.domain.ramen.repository.RamenTradeRepository
+import com.example.ramenbm.domain.ramen.util.RamenTradeConverter
 import com.example.ramenbm.domain.user.presentation.data.dto.UserQueryDto
 import com.example.ramenbm.domain.user.service.UserAccountService
 import com.example.ramenbm.domain.user.util.AccountConverter
@@ -10,12 +13,22 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class UserAccountServiceImpl(
 	private val userUtil: UserUtil,
-	private val accountConverter: AccountConverter
+	private val accountConverter: AccountConverter,
+	private val ramenTradeRepository: RamenTradeRepository,
+	private val ramenTradeConverter: RamenTradeConverter
 ) : UserAccountService {
 
 	@Transactional(readOnly = true, rollbackFor = [Exception::class])
 	override fun findUserByIdx(idx: Long): UserQueryDto =
 		userUtil.findUserByIdx(idx)
 			.let { accountConverter.toQueryDto(it) }
+
+	@Transactional(readOnly = true, rollbackFor = [Exception::class])
+	override fun findMyRamenTradeByUser(): RamenTradeListQueryDto {
+		val user = userUtil.currentUser()
+		ramenTradeRepository.findRamenTradeByUser(user)
+			.let { ramenTradeConverter.toListQueryDto(it) }
+			.let { return RamenTradeListQueryDto(listOf(it)) }
+	}
 
 }
